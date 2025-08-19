99092d68c4c3ff3f11daab9f97fadaaaf85351d7
henokworkuliyew <hw93gojj@gmail.com>
2025-08-19 12:37:05 +0300

    Update  page with improved image upload handling

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,7 +1,13 @@
-
 import { createRouteHandler } from 'uploadthing/next'
 import { ourFileRouter } from './core'
 
+console.log('[v0] UPLOADTHING_TOKEN exists:', !!process.env.UPLOADTHING_TOKEN)
+console.log('[v0] Token length:', process.env.UPLOADTHING_TOKEN?.length)
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    token: process.env.UPLOADTHING_TOKEN,
+    logLevel: 'Info',
+  },
 })
